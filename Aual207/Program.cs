d1d211a42871671431ed5f54be37a2836a9d8965
Pedro Lucas <pedro.lima3@estudante.ifms.edu.br>
2021-10-18 00:52:41 -0300

    normal

---
+++ Aual207/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aual207
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Maria";
+            string b = "Alex";
+
+            Console.WriteLine(a.Equals(b));
+            //OUTPUT: false;
+
+            string c = "Jhon";
+            string d = "Bob";
+
+            Console.WriteLine(c.GetHashCode());
+            Console.WriteLine(d.GetHashCode());
+            //This will return a int value;
+        }
+    }
+}
